f15ed6685bc41652f20ef7b0e04d7af3b7f9bde4
Lou-Anne <101814973+Lou-Anne0@users.noreply.github.com>
2023-01-23 13:16:39 +0100

    first commit
    Arena + Main menu

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour
+{
+    public int size = 100;
+
+    private Cell[,] _cellgrid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cellgrid = new Cell[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = new Cell();
+                cell.isWater = true;
+                _cellgrid[x, y] = cell;
+            }
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        for (int y = 0; y < size; y++)
+        {
+
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = _cellgrid[x, y];
+
+                if (cell.isWater)
+                {
+                    Gizmos.color = Color.blue;
+                }
+                else
+                {
+                    Gizmos.color = Color.green;
+                }
+
+                Vector3 pos = new Vector3(x, 0, y);
+                Gizmos.DrawCube(pos, Vector3.one);
+            }
+        }
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Réglages des Volumes")] 
+    [SerializeField] private TMP_Text volumeEffetsTextvalue = null;
+    [SerializeField] private Slider volumeEffetSlider = null;
+
+    [SerializeField] private GameObject comfirmationPromt = null;
+    [SerializeField] private float defaultVolume = 50f;
+    
+    
+    
+    
+    [Header("Lancement de Jeu")] 
+    public string _lancerJeu;
+
+
+
+    [Header("Graphismes")] 
+    private int _qualityLevel;
+
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+
+
+    [Header("Resolution")] 
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+                
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void LancerJeu()
+    {
+        SceneManager.LoadScene(_lancerJeu);
+    }
+
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetFloat("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel((_qualityLevel));
+        StartCoroutine(ConfirmationBox());
+    }
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeEffetsTextvalue.text = volume.ToString("N");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume",AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeEffetSlider.value = defaultVolume;
+            volumeEffetsTextvalue.text = defaultVolume.ToString("N");
+            VolumeApply();
+        }
+        
+        if (MenuType == "Graphics")
+        {
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+            GraphicsApply();
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GraphicsApply();
+        }
+            
+    }
+    
+    
+    public IEnumerator ConfirmationBox()
+    {
+        comfirmationPromt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        comfirmationPromt.SetActive(false);
+    }
+}
